552557fc01ae6c273914f1ba5ded65307ba458f9
zjsywcc <zjsywcc@163.com>
2016-09-30 10:51:57 +0800

    add problem 77

--- DynamicProgrammingB/77/src/Main.java
+++ DynamicProgrammingB/77/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str1 = "www.lintcode.com code";
+        String str2 = "www.ninechapter.com code";
+        System.out.println(longestCommonSubsequence(str1, str2));
     }
 
-    public int longestCommonSubsequence(String A, String B) {
+    public static int longestCommonSubsequence(String A, String B) {
         int len1 = A.length();
         int len2 = B.length();
         int[][] dp = new int[len1 + 1][len2 + 1];
@@ -14,12 +16,15 @@ public class Main {
         for(int j = 0; j < len2 + 1; j++) {
             dp[0][j] = 0;
         }
-        for(int i = 0; i < len1 + 1; i++) {
-            for(int j = 0; j < len2 + 1; j++) {
-                if(A.charAt(i) == B.charAt(j)) {
-                    dp[i][j] = dp[i]
+        for(int i = 1; i < len1 + 1; i++) {
+            for(int j = 1; j < len2 + 1; j++) {
+                if(A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(Math.max(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
                 }
             }
         }
+        return dp[len1][len2];
     }
 }
